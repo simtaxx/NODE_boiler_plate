6e32314c5c9e41778292878cc19320148edf427e
simtaxx <souici.haris@gmail.com>
2019-05-15 15:26:24 +0200

    index

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 /*
-Importer les composants  serveur
+Importer les composants serveur
 */
+// NodeJS
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+
+// Inner
+const frontRouter = require("./routes/front.router");
 //
 
 /*
 Configuration du serveur
 */
-//Définir les variables serveur
+// Définir les variables serveur
 const server = express();
 const port = process.env.PORT;
+
+// Configuration du moteur de rendu
+server.set("view engine", "ejs");
 
 // Définition du dossier static du client
 server.set("views", __dirname + "/www");
 server.use(express.static(path.join(__dirname, "www")));
-//
+
+// Utilisation des routers
+server.use("/", frontRouter);
 //
 
-/*
+/* 
 Lancer le serveur
 */
 server.listen(port, () => console.log(`Server listening on port ${port}`));
